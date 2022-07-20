de43d14af3c060950209ef98f47ff5f15acbc473
Hieu. Tran Trung - CMC Global DE1 <tthieu8@cmcglobal.vn>
2022-07-20 17:32:52 +0700

    update some rabbitMQ

--- User/Routes/queryRoute.js
+++ User/Routes/queryRoute.js
@@ -8,14 +8,49 @@ const io = require('socket.io-client')
 const socket = io('http://localhost:3000')
 socket.emit('query')
 
-socket.on('saveUser', msg => {
+/*socket.on('saveUser', msg => {
     queryDemoDB.saveUser(msg)
-})
+})*/
 socket.on('updateUserBalance', msg => {
     queryDemoDB.updateBalance(msg)
 })
 
 router.get('/users', queryDemoDB.getAllUsers)
 router.get('/user:id', queryDemoDB.getUser)
+
+
+const amqp = require('amqplib/callback_api')
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+  
+    channel.assertExchange('testing','direct', {
+      durable: false
+    });
+    
+    channel.assertQueue('', {exclusive: true},function(error2, q){
+        if(error2){
+            throw error2
+        }
+        //channel.prefetch(1)
+        channel.bindQueue(q.queue, 'testing','main')
+        channel.consume(q.queue, function(msg) {
+            const data = JSON.parse(msg.content)
+            console.log(data)
+            queryDemoDB.saveUser(data)
+        }, {
+            noAck: true
+        });
+    })
+    
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -25,36 +25,3 @@ io.on('connection', socket=>{
     })
     
 })
-
-const amqp = require('amqplib/callback_api')
-
-amqp.connect('amqp://localhost', function(error0, connection) {
-  if (error0) {
-    throw error0;
-  }
-  
-  connection.createChannel(function(error1, channel) {
-    if (error1) {
-      throw error1;
-    }
-  
-    channel.assertExchange('testing','direct', {
-      durable: false
-    });
-    
-    channel.assertQueue('', {exclusive: true},function(error2, q){
-        if(error2){
-            throw error2
-        }
-        channel.prefetch(1)
-        channel.bindQueue(q.queue, 'testing','main')
-        channel.consume(q.queue, function(msg) {
-            const r = JSON.parse(msg.content)
-            console.log(r)
-        }, {
-            noAck: true
-        });
-    })
-    
-  });
-});
